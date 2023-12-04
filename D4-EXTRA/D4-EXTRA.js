39afc36b99cca394c103014d665c70dc05c5dd84
Chiara Puleio <chiarapuleio@yahoo.it>
2023-12-04 18:28:24 +0100

    -

--- D4-EXTRA/D4-EXTRA.js
+++ D4-EXTRA/D4-EXTRA.js
@@ -6,69 +6,139 @@
  La funzione deve inoltre ritornare la somma di tutti i valori maggiori di 5.
 */
 
-function giveMeRandom (n){
-    let randomArr = []
-    for(let i = 0; i<n; i++){
-        randomArr.push(Math.floor(Math.random() * 10))
-    }
-    return randomArr
+function giveMeRandom(n) {
+  let randomArr = [];
+  for (let i = 0; i < n; i++) {
+    randomArr.push(Math.floor(Math.random() * 10));
+  }
+  return randomArr;
 }
-console.log(giveMeRandom(5))
+console.log(giveMeRandom(5));
 
 const checkArray = function (randomCasualArr) {
-    let numSum = 0 
-    for(let i=0; i<randomCasualArr.length; i++){
-        if (randomCasualArr[i]>5){
-            console.log(randomCasualArr[i] + " maggiore di 5")
-            numSum+=randomCasualArr[i]
-        } else {
-            console.log(randomCasualArr[i] + " minore di 5")
-        }
+  let numSum = 0;
+  for (let i = 0; i < randomCasualArr.length; i++) {
+    if (randomCasualArr[i] > 5) {
+      console.log(randomCasualArr[i] + " maggiore di 5");
+      numSum += randomCasualArr[i];
+    } else {
+      console.log(randomCasualArr[i] + " minore di 5");
     }
-    return numSum
-}
-console.log(checkArray(giveMeRandom(5)))
+  }
+  return numSum;
+};
+console.log(checkArray(giveMeRandom(5)));
 
 /* EXTRA 2
  Nel tuo eCommerce disponi di un'array di oggetti chiamato "shoppingCart". Ognuno di questi oggetti ha le seguenti proprietà: "price", "name", "id" e "quantity".
  Crea una funzione chiamata "shoppingCartTotal" che calcola il totale dovuto al negozio (tenendo conto delle quantità di ogni oggetto).
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+const shoppingCart = [
+  {
+    Name: "Fumetto",
+    Price: 5,
+    Id: "FMA0310",
+    Quantity: 3,
+  },
+  {
+    Name: "Action Figure",
+    Price: 35,
+    Id: "ELR0610",
+    Quantity: 2,
+  },
+  {
+    Name: "Collection Cards Pack",
+    Price: 3.5,
+    Id: "MTG0131",
+    Quantity: 4,
+  },
+];
+
+const shoppingCartTotal = function (totalExp) {
+  let totalCart = 0;
+  for (let i = 0; i < shoppingCart.length; i++) {
+    shoppingCart[i].Price * shoppingCart[i].Quantity;
+    totalCart += shoppingCart[i].Price * shoppingCart[i].Quantity;
+  }
+  return totalCart;
+};
+console.log(shoppingCartTotal());
 
 /* EXTRA 3
  Nel tuo eCommerce disponi di un'array di oggetti chiamato "shoppingCart". Ognuno di questi oggetti ha le seguenti proprietà: "price", "name", "id" e "quantity".
  Crea una funzione chiamata "addToShoppingCart" che riceve un nuovo oggetto dello stesso tipo, lo aggiunge a "shoppingCart" e ritorna il nuovo numero totale degli elementi.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+let addToShoppingCart = function (newCart) {};
 
 /* EXTRA 4
  Nel tuo eCommerce disponi di un'array di oggetti chiamato "shoppingCart". Ognuno di questi oggetti ha le seguenti proprietà: "price", "name", "id" e "quantity".
  Crea una funzione chiamata "maxShoppingCart" che riceve l'array "shoppingCart" e ritorna l'oggetto più costoso in esso contenuto.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+function maxShoppingCart(shoppingCart) {
+  let max = shoppingCart[0];
+
+  for (let i = 1; i < shoppingCart.length; i++) {
+    if (shoppingCart[i].price > max.price) {
+      max = shoppingCart[i];
+    }
+  }
+  return max;
+}
+console.log(maxShoppingCart());
 
 /* EXTRA 5
  Nel tuo eCommerce disponi di un'array di oggetti chiamato "shoppingCart". Ognuno di questi oggetti ha le seguenti proprietà: "price", "name", "id" e "quantity".
  Crea una funzione chiamata "latestShoppingCart" che riceve l'array "shoppingCart" e ritorna l'ultimo elemento.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+function latestShoppingCart(shoppingCart) {
+  return shoppingCart[shoppingCart.length - 1];
+}
+console.log(latestShoppingCart());
 
 /* EXTRA 6
  Crea una funzione chiamata "loopUntil" che riceve un numero intero come parametro con valore tra 0 e 9.
  La funzione è composta da un ciclo che stampa un numero casuale tra 0 e 9 finchè il numero casuale non è maggiore di x per tre volte di fila.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+function loopUntil(x) {
+  let counter = 0;
+  while (counter < 3) {
+    let randomNumber = Math.floor(Math.random() * 10);
+
+    if (randomNumber > x) {
+      console.log("Numero casuale maggiore di x:", randomNumber);
+      counter++;
+    } else {
+      console.log("Numero casuale non maggiore di x:", randomNumber);
+    }
+  }
+}
+console.log(loopUntil(5));
 
 /* EXTRA 7
 Crea una funzione chiamata "average" che riceve un array come parametro e ne ritorna la media aritmetica. La funzione salta automaticamente i valori non numerici nell'array.
 */
+const array = [1, 2, 3];
+function average(array) {
+  let sum = 0;
+  let count = 0;
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+  for (let i = 0; i < array.length; i++) {
+    if (typeof array[i] === "number") {
+      sum += array[i];
+      count++;
+    }
+  }
+  if (count === 0) {
+    return 0;
+  }
+  return sum / count;
+}
+console.log(average(array));
 
 /* EXTRA 8
  Crea una funzione chiamata "longest" che trova la stringa più lunga all'interno di un array di stringhe fornito come parametro.
--- D4.js
+++ D4.js
@@ -2,11 +2,11 @@
  Scrivi una funzione di nome "area", che riceve due parametri (l1, l2) e calcola l'area del rettangolo associato..
 */
 
-function area(l1,l2){
-    const totalArea = l1*l2
-    return totalArea
+function area(l1, l2) {
+  const totalArea = l1 * l2;
+  return totalArea;
 }
-console.log(area(5,9))
+console.log(area(5, 9));
 
 /* ESERCIZIO 2
  Scrivi una funzione di nome "crazySum", che riceve due numeri interi come parametri.
@@ -14,30 +14,30 @@ console.log(area(5,9))
  la loro somma moltiplicata per tre.
 */
 
-function crazySum (int1, int2){
-    const totalSum = int1+int2
-    if(int1!=int2){
-        return totalSum 
-    } else if (int1===int2){
-        return totalSum*3
-    }
+function crazySum(int1, int2) {
+  const totalSum = int1 + int2;
+  if (int1 != int2) {
+    return totalSum;
+  } else if (int1 === int2) {
+    return totalSum * 3;
+  }
 }
-console.log(crazySum(3,3))
+console.log(crazySum(3, 3));
 
 /* ESERCIZIO 3
  Scrivi una funzione di nome "crazyDiff" che calcola la differenza assoluta tra un numero fornito come parametro e 19.
  Deve inoltre tornare la differenza assoluta moltiplicata per tre qualora il numero fornito sia maggiore di 19.
 */
 
-// function crazyDiff (num1){
-//     const totalDiff = num1-19
-//     if(num1>19){
-//         return totalDiff*3
-//     } else if (num1<19){
-//         return totalSum
-//     }
-// }
-// console.log(crazyDiff(57))
+function crazyDiff(num1) {
+  const totalDiff = Math.abs(num1 - 19);
+  if (num1 > 19) {
+    return totalDiff * 3;
+  } else if (num1 < 19) {
+    return totalDiff;
+  }
+}
+console.log(crazyDiff(57));
 
 //MATH.ABS
 
@@ -45,18 +45,18 @@ console.log(crazySum(3,3))
  Scrivi una funzione di nome "boundary" che accetta un numero intero n come parametro, e ritorna true se n è compreso tra 20 e 100 (incluso) oppure
  se n è uguale a 400.
 */
- let n1 = 20
- let n2 = 100
- let n3 = 400
+let n1 = 20;
+let n2 = 100;
+let n3 = 400;
 
-function boundary (n){
-    if(n>n1 && n<n2 || n===n3){
-        return true
-    } else {
-        return false
-    }
+function boundary(n) {
+  if ((n > n1 && n < n2) || n === n3) {
+    return true;
+  } else {
+    return false;
+  }
 }
-console.log(boundary(450))
+console.log(boundary(450));
 
 /* ESERCIZIO 5
  Scrivi una funzione di nome "epify" che accetta una stringa come parametro.
@@ -64,89 +64,81 @@ console.log(boundary(450))
  ritornare la stringa originale senza alterarla.
 */
 
-
-const epify = function (finalTestText){
-    if(finalTestText.startsWith("Epicode")){
-        return finalTestText
-    } else {
-        return "Epicode".concat(finalTestText)
-    }
-}
-console.log(epify(" Hello"))
-
-
+const epify = function (finalTestText) {
+  if (finalTestText.startsWith("Epicode")) {
+    return finalTestText;
+  } else {
+    return "Epicode".concat(finalTestText);
+  }
+};
+console.log(epify(" Hello"));
 
 /* ESERCIZIO 6
  Scrivi una funzione di nome "check3and7" che accetta un numero positivo come parametro. La funzione deve controllare che il parametro sia un multiplo
  di 3 o di 7. (Suggerimento: usa l'operatore modulo)
 */
 
-function check3and7 (numPos){
-    return numPos%3===0 || numPos%7===0
-    // if(numPos % 3 === 0){
-    //     return "è multiplo di 3"
-    // } else if (numPos % 7 === 0){
-    //     return "è multiplo di 7"
-    // }
+function check3and7(numPos) {
+  return numPos % 3 === 0 || numPos % 7 === 0;
+  // if(numPos % 3 === 0){
+  //     return "è multiplo di 3"
+  // } else if (numPos % 7 === 0){
+  //     return "è multiplo di 7"
+  // }
 }
-console.log(check3and7(49))
-
+console.log(check3and7(49));
 
 /* ESERCIZIO 7
  Scrivi una funzione di nome "reverseString", il cui scopo è invertire una stringa fornita come parametro (es. "EPICODE" --> "EDOCIPE")
 */
 
 const reverseString = function (str) {
-    let splitString = str.split("")
-    let reverseArray = splitString.reverse() 
-    let joinArray = reverseArray.join("")
-    return joinArray
-}
-console.log(reverseString("EPICODE"))
+  let splitString = str.split("");
+  let reverseArray = splitString.reverse();
+  let joinArray = reverseArray.join("");
+  return joinArray;
+};
+console.log(reverseString("EPICODE"));
 
 /* ESERCIZIO 8
  Scrivi una funzione di nome "upperFirst", che riceve come parametro una stringa formata da diverse parole.
  La funzione deve rendere maiuscola la prima lettera di ogni parola contenuta nella stringa.
 */
 
-let lowerText = "Luke, sono tuo padre"
+let lowerText = "Luke, sono tuo padre";
 
-function upperFirst (upperText){
-    let lowerArr = lowerText.split(" ")
-    for(let i=0; i<lowerArr.length; i++){
-        lowerArr[i]= lowerArr[i].replace(lowerArr[i].charAt(0), lowerArr[i].charAt(0).toUpperCase())
-    }
-    upperText = lowerArr.join(" ")
-    return upperText
+function upperFirst(upperText) {
+  let lowerArr = lowerText.split(" ");
+  for (let i = 0; i < lowerArr.length; i++) {
+    lowerArr[i] = lowerArr[i].replace(lowerArr[i].charAt(0), lowerArr[i].charAt(0).toUpperCase());
+  }
+  upperText = lowerArr.join(" ");
+  return upperText;
 }
-console.log(upperFirst("Luke, sono tuo padre"))
-
-
+console.log(upperFirst("Luke, sono tuo padre"));
 
 /* ESERCIZIO 9
  Scrivi una funzione di nome "cutString", che riceve come parametro una stringa. La funzione deve creare una nuova stringa senza il primo e l'ultimo carattere
  della stringa originale.
 */
 
-let completeString = "Testo da tagliare"
+let completeString = "Testo da tagliare";
 
-function cutString(cuttedString){
-    cuttedString = completeString.substring(1,16)
-    return cuttedString
+function cutString(cuttedString) {
+  cuttedString = completeString.substring(1, 16);
+  return cuttedString;
 }
-console.log(cutString())
-
+console.log(cutString());
 
 /* ESERCIZIO 10
  Scrivi una funzione di nome "giveMeRandom", che accetta come parametro un numero n e ritorna un'array contenente n numeri casuali inclusi tra 0 e 10.
 */
 
-
-function giveMeRandom (n){
-    let randomArr = []
-    for(let i = 0; i<n; i++){
-        randomArr.push(Math.floor(Math.random() * 10))
-    }
-    return randomArr
+function giveMeRandom(n) {
+  let randomArr = [];
+  for (let i = 0; i < n; i++) {
+    randomArr.push(Math.floor(Math.random() * 11));
+  }
+  return randomArr;
 }
-console.log(giveMeRandom(5))
+console.log(giveMeRandom(5));
